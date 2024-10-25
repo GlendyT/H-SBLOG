fb2b184517a8c188acf5815f5bcdcdd1508f4931
GlendyT <glendytuyuc@gmail.com>
2024-10-25 09:25:03 -0600

    first changes of blog, only frontedn

--- src/app/components/categoryList/CategoryList.jsx
+++ src/app/components/categoryList/CategoryList.jsx
@@ -1,7 +1,26 @@
-import styles from "./categoryList.module.css"
+import Link from "next/link";
+import styles from "./categoryList.module.css";
+import Image from "next/image";
 
 export const CategoryList = () => {
   return (
-    <div>CategoryList</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.style}`}
+        >
+          <Image
+            src="/style.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          Style
+        </Link>
+      </div>
+    </div>
+  );
+};
