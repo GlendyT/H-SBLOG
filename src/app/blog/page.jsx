6a7c2542c479f8d9e7f6ae110a3ff7490c6377fc
GlendyT <glendytuyuc@gmail.com>
2024-10-31 23:00:44 -0600

    finishing project, adding or removing features tomorrow 01nov

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,17 +1,19 @@
-import { CardList } from "../components/cardList/CardList"
-import { Menu } from "../components/menu/Menu"
-import styles from "./blogPage.module.css"
+import { CardList } from "../components/cardList/CardList";
+import { Menu } from "../components/menu/Menu";
+import styles from "./blogPage.module.css";
 
-const BlogPage = () => {
+const BlogPage = ({ searchParams }) => {
+  const page = parseInt(searchParams.page) || 1;
+  const { cat } = searchParams;
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Blog Page</h1>
+      <h1 className={styles.title}>{cat} Page</h1>
       <div className={styles.content}>
-        <CardList/>
-        <Menu/>
+        <CardList page={page} cat={cat} />
+        <Menu />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default BlogPage
+export default BlogPage;
